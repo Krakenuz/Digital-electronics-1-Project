c6a611ceff59e19cc36ff1b7fd6993f263ddd374
unknown <Krakenuz>
2021-04-14 20:50:47 +0200

    Added display_control

---
+++ Door lock system/Door lock system.srcs/sim_1/new/tb_display_control.vhd
@@ -0,0 +1,138 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engeer: 
+-- 
+-- Create Date: 14.04.2021 15:52:49
+-- Design Name: 
+-- Module Name: tb_display_control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the followg library declaration if usg
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the followg library declaration if stantiatg
+-- any Xilx leaf cells  this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_display_control is
+--  Port ( );
+end tb_display_control;
+
+architecture testbench of tb_display_control is
+    signal s_Button_0 :  STD_LOGIC;
+    signal s_Button_1 :  STD_LOGIC;
+    signal s_Button_2 :  STD_LOGIC;
+    signal s_Button_3 :  STD_LOGIC;
+    signal s_Button_4 :  STD_LOGIC;
+    signal s_Button_5 :  STD_LOGIC;
+    signal s_Button_6 :  STD_LOGIC;
+    signal s_Button_7 :  STD_LOGIC;
+    signal s_Button_8 :  STD_LOGIC;
+    signal s_Button_9 :  STD_LOGIC;
+    signal s_Button_RESET :  STD_LOGIC;
+    signal s_Button_SET :  STD_LOGIC;         
+    signal s_Display_1 :  std_logic_vector(4 - 1 downto 0);
+    signal s_Display_2 :  std_logic_vector(4 - 1 downto 0);
+    signal s_Display_3 :  std_logic_vector(4 - 1 downto 0);
+    signal s_Display_4 :  std_logic_vector(4 - 1 downto 0);
+
+begin
+	uut_display_control : entity work.display_control
+        port map(
+            Button_0	=>	s_Button_0,
+            Button_1	=>	s_Button_1,
+            Button_2	=>  s_Button_2,
+            Button_3	=>	s_Button_3,
+            Button_4	=>	s_Button_4,
+            Button_5	=>	s_Button_5,
+            Button_6	=>	s_Button_6,
+            Button_7	=>	s_Button_7,
+            Button_8	=>	s_Button_8,
+            Button_9	=>	s_Button_9,
+            Button_RESET	=>	s_Button_RESET,
+            Button_SET	=>	s_Button_SET,
+           Display_1	=>	s_Display_1,
+           Display_2	=>	s_Display_2,
+           Display_3	=>	s_Display_3,
+           Display_4	=>	s_Display_4);
+
+	p_stimulus : process
+    begin
+        report "Stimulus process started" severity note;
+        s_Button_1 <= '0';
+        wait for 10ns;
+        s_Button_1 <= '1';
+        s_Button_0 <= '0';
+        s_Button_2 <= '0';
+        s_Button_3 <= '0';
+        s_Button_4 <= '0';
+        s_Button_5 <= '0';
+        s_Button_6 <= '0';
+        s_Button_7 <= '0';
+        s_Button_8 <= '0';
+        s_Button_9 <= '0';
+        s_Button_RESET <= '0';
+        s_Button_SET <= '0';
+		wait for 50ns;
+		s_Button_1 <= '0';
+		wait for 10ns;
+		s_Button_1 <= '1';
+		wait for 50ns;
+		s_Button_1 <= '0';
+		wait for 10ns;
+		
+		s_Button_9 <= '1';
+		wait for 50ns;
+		s_Button_9 <= '0';
+		wait for 10ns;
+		
+		s_Button_6 <= '1';
+		wait for 50ns;
+		s_Button_6 <= '0';
+		wait for 10ns;
+		
+		s_Button_RESET <= '1';
+		wait for 50ns;
+		s_Button_RESET <= '0';
+		wait for 10ns;
+		
+		s_Button_4 <= '1';
+		wait for 50ns;
+		s_Button_4 <= '0';
+		wait for 10ns;
+		
+		s_Button_5 <= '1';
+		wait for 50ns;
+		s_Button_5 <= '0';
+		wait for 10ns;
+		
+		s_Button_8 <= '1';
+		wait for 50ns;
+		s_Button_8 <= '0';
+		wait for 10ns;
+		
+		s_Button_3 <= '1';
+		wait for 50ns;
+		s_Button_3 <= '0';
+		wait for 10ns;
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+end testbench;
---
+++ Door lock system/Door lock system.srcs/sources_1/new/display_control.vhd
@@ -0,0 +1,215 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.04.2021 12:48:14
+-- Design Name: 
+-- Module Name: display_control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity display_control is
+    Port ( Button_0 : in STD_LOGIC;
+            Button_1 : in STD_LOGIC;
+            Button_2 : in STD_LOGIC;
+            Button_3 : in STD_LOGIC;
+            Button_4 : in STD_LOGIC;
+            Button_5 : in STD_LOGIC;
+            Button_6 : in STD_LOGIC;
+            Button_7 : in STD_LOGIC;
+            Button_8 : in STD_LOGIC;
+            Button_9 : in STD_LOGIC;
+            Button_RESET : in STD_LOGIC;
+            Button_SET : in STD_LOGIC;         
+           Display_1 : out std_logic_vector(4 - 1 downto 0);
+           Display_2 : out std_logic_vector(4 - 1 downto 0);
+           Display_3 : out std_logic_vector(4 - 1 downto 0);
+           Display_4 : out std_logic_vector(4 - 1 downto 0));
+end display_control;
+
+architecture Behavioral of display_control is
+    -- Pomocný vektor tlaèítka
+    signal Buttons  : std_logic_vector(12 - 1 downto 0);
+    -- 
+    signal cnt : natural;
+begin
+p_display_control: process(Button_0,Button_1,Button_2,Button_3,Button_4,Button_5,Button_6,Button_7,Button_8,Button_9,Button_Reset,Button_SET)
+begin 
+    Buttons(0) <= Button_0;
+    Buttons(1) <= Button_1;
+    Buttons(2) <= Button_2;
+    Buttons(3) <= Button_3;
+    Buttons(4) <= Button_4;
+    Buttons(5) <= Button_5;
+    Buttons(6) <= Button_6;
+    Buttons(7) <= Button_7;
+    Buttons(8) <= Button_8;
+    Buttons(9) <= Button_9;
+    Buttons(10) <= Button_RESET;
+    Buttons(11) <= Button_SET;
+    
+    if (rising_edge(Button_0) or rising_edge(Button_1)or rising_edge(Button_2)or rising_edge(Button_3)or rising_edge(Button_4)or rising_edge(Button_5)or rising_edge(Button_6)or rising_edge(Button_7)or rising_edge(Button_8)or rising_edge(Button_9)or rising_edge(Button_RESET)or rising_edge(Button_SET)) then
+        cnt <=cnt+1; 
+        
+    end if;
+    
+    case Buttons is
+        when "000000000001" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0000";
+                when 2 =>
+                    Display_2<="0000";
+                when 3 =>
+                    Display_3<="0000";
+                when 4 =>
+                    Display_4<="0000";
+                when others =>    
+            end case;
+        when "000000000010" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0001";
+                when 2 =>
+                    Display_2<="0001";
+                when 3 =>
+                    Display_3<="0001";
+                when 4 =>
+                    Display_4<="0001";
+                    when others =>   
+            end case;   
+        when "000000000100" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0010";
+                when 2 =>
+                    Display_2<="0010";
+                when 3 =>
+                    Display_3<="0010";
+                when 4 =>
+                    Display_4<="0010";
+                    when others =>   
+            end case;   
+        when "000000001000" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0011";
+                when 2 =>
+                    Display_2<="0011";
+                when 3 =>
+                    Display_3<="0011";
+                when 4 =>
+                    Display_4<="0011";
+                    when others =>   
+            end case;   
+        when "000000010000" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0100";
+                when 2 =>
+                    Display_2<="0100";
+                when 3 =>
+                    Display_3<="0100";
+                when 4 =>
+                    Display_4<="0100";
+                    when others =>   
+            end case;   
+        when "000000100000" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0101";
+                when 2 =>
+                    Display_2<="0101";
+                when 3 =>
+                    Display_3<="0101";
+                when 4 =>
+                    Display_4<="0101";
+                    when others =>   
+            end case;   
+        when "000001000000" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0110";
+                when 2 =>
+                    Display_2<="0110";
+                when 3 =>
+                    Display_3<="0110";
+                when 4 =>
+                    Display_4<="0110";
+                    when others =>   
+            end case;   
+        when "000010000000" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="0111";
+                when 2 =>
+                    Display_2<="0111";
+                when 3 =>
+                    Display_3<="0111";
+                when 4 =>
+                    Display_4<="0111";
+                    when others =>   
+            end case;   
+        when "000100000000" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="1000";
+                when 2 =>
+                    Display_2<="1000";
+                when 3 =>
+                    Display_3<="1000";
+                when 4 =>
+                    Display_4<="1000";
+                    when others =>   
+            end case;   
+        when "001000000000" =>
+            case cnt is
+                when 1 =>
+                    Display_1<="1001";
+                when 2 =>
+                    Display_2<="1001";
+                when 3 =>
+                    Display_3<="1001";
+                when 4 =>
+                    Display_4<="1001";
+                    when others =>   
+            end case;   
+        when "010000000000" =>      --RESET
+            Display_1<="0000";
+            Display_2<="0000";
+            Display_3<="0000";
+            Display_4<="0000";
+            cnt<=0;
+        when "100000000000" =>     --SET
+            --domyslet
+        when others =>   
+        
+    end case;   
+    --if (cnt >=4) then         --Pøepisovat èíslice bez resetu
+      --      cnt<=0;
+      --  end if;                                                                                                          
+end process p_display_control;
+end Behavioral;
